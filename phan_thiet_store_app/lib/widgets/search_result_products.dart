45838cb97bf138d84f50164eee04fc77fad47aeb
Khoi Le <le.khoi@flynk.com>
2018-04-03 23:40:09 +0700

    Search result UI

---
+++ phan_thiet_store_app/lib/widgets/search_result_products.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/product_card.dart';
+
+class WidgetSearchResultProducts extends StatelessWidget {
+  WidgetSearchResultProducts({this.searchStr});
+
+  final searchStr;
+
+  @override
+  Widget build(BuildContext context) {
+    return new ListView(
+        children: <Widget>[
+          new Container(
+              child: new Text("Kết quả tìm kiếm cho '${this.searchStr}'",
+                  style: new TextStyle(fontWeight: FontWeight.bold)),
+              padding: new EdgeInsets.only(left: 5.0)),
+          new GridView.count(
+              primary: false,
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(5.0),
+              crossAxisSpacing: 5.0,
+              crossAxisCount: 2,
+              children: <Widget>[
+                new WidgetProductCard(),
+                new WidgetProductCard(),
+                new WidgetProductCard(),
+                new WidgetProductCard(),
+                new WidgetProductCard(),
+                new WidgetProductCard(),
+                new WidgetProductCard(),
+                new WidgetProductCard(),
+                new WidgetProductCard()
+              ]
+          )
+        ]);
+  }
+}
