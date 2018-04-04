0a90cee740b4d43eee231bf6f03017e6c55df8dd
Khoi Le <le.khoi@flynk.com>
2018-04-04 23:19:29 +0700

    Designing cart page

--- phan_thiet_store_app/lib/pages/cart.dart
+++ phan_thiet_store_app/lib/pages/cart.dart
@@ -1,12 +1,55 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/cart_item.dart';
+
 class PageCart extends StatelessWidget {
+  void checkoutCart() {
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(title: new Text('Giỏ hàng')),
-      body: new Text('Cart page')
-    );
+        appBar: new AppBar(title: new Text('Giỏ hàng')),
+        body: new Column(
+          children: <Widget>[
+            new Expanded(
+                child: new ListView(children: <Widget>[
+              new WidgetCartItem(),
+              new WidgetCartItem(),
+              new WidgetCartItem(),
+              new WidgetCartItem(),
+              new WidgetCartItem(),
+              new WidgetCartItem(),
+              new WidgetCartItem()
+            ], padding: new EdgeInsets.all(8.0))),
+            new Card(
+                child: new Container(
+                    child: new Row(
+                      children: <Widget>[
+                        new Expanded(
+                            child: new Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            new Container(
+                                child: new Text('Tổng cộng',
+                                    style: Theme.of(context).textTheme.caption),
+                                padding: new EdgeInsets.all(4.0)),
+                            new Container(
+                                child: new Text('đ 800.000',
+                                    style: Theme.of(context).textTheme.title),
+                                padding: new EdgeInsets.all(4.0))
+                          ],
+                        )),
+                        new RaisedButton(
+                            color: Theme.of(context).accentColor,
+                            textTheme: ButtonTextTheme.primary,
+                            child: new Text('Đặt hàng'), onPressed: checkoutCart
+                        )
+                      ],
+                    ),
+                    padding: new EdgeInsets.all(16.0)))
+          ],
+        ));
   }
 }
---
+++ phan_thiet_store_app/lib/widgets/cart_item.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class WidgetCartItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Card(
+      child: new Container(
+        child: new Row(children: <Widget>[
+          new Container(
+            child: new Image.asset('images/tai_nghe.png', fit: BoxFit.cover),
+            padding: new EdgeInsets.all(5.0),
+            height: 64.0,
+          ),
+          new Expanded(
+              child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Container(
+                  child: new Text('Tai nghe ANZ'),
+                  padding: new EdgeInsets.all(5.0)),
+              new Container(
+                  child: new Text('đ 280.000',
+                      style: Theme.of(context).textTheme.caption),
+                  padding: new EdgeInsets.all(5.0))
+            ],
+          )),
+          new Row(
+            children: <Widget>[
+              new IconButton(
+                  icon: new Icon(Icons.remove_circle), onPressed: null),
+              new Text('2'),
+              new IconButton(icon: new Icon(Icons.add_circle), onPressed: null)
+            ],
+          ),
+        ]),
+        padding: new EdgeInsets.all(12.0),
+        color: Theme.of(context).cardColor,
+      ),
+    );
+  }
+}
